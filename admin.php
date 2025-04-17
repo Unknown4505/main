d11e8a2f4aa43b79275c76711f892adadda1aeaa
lehuukhang <lehuukhang147@gmail.com>
2025-04-17 19:48:28 +0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,9 +1,6 @@
 <?php
 session_start();
-if (!isset($_SESSION['admin_id'])) {
-    header("Location: login-admin.php");
-    exit;
-}
+
 $servername = "localhost";
 $username = "root";
 $password = "";
