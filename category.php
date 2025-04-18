722103e2a1395c15f844d5b771e19dd4325dc7c5
thien33 <pnt1915@gmail.com>
2025-04-18 21:00:48 +0700

    Update category.php

--- category.php
+++ category.php
@@ -313,77 +313,7 @@ include 'header.php';
 <!-- End Filter Bar -->
 
 <!-- Start footer Area -->
-<footer class="footer-area section_gap">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-3 col-md-6 col-sm-6">
-                <div class="single-footer-widget">
-                    <h6>Về Chúng Tôi</h6>
-                    <p>
-                        “Kể từ lúc thành lập vào năm 2012, Karma luôn được khách hàng đánh giá là một trong những cửa hang giày chất lượng cao tại Việt Nam. Hiện tại, Karma vẫn tiếp tục duy trì chất lượng dịch vụ và sản phẩm tốt để gìn giữ sự hài lòng của khách hàng.”
-                    </p>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-6">
-                <div class="single-footer-widget">
-                    <h6>Bảng tin</h6>
-                    <p>Luôn cập nhật thông tin mới nhất của chúng tôi
-                    </p>
-                    <div class="">
-                        <form target="_blank" novalidate="true" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01"
-                              method="get" class="form-inline">
-                            <div class="form-group lbel-inline">
-                                <input type="email" class="form-control" name="EMAIL" placeholder="Nhập email" required>
-                            </div>
-                            <button class="btn btn-default">
-                                <span class="lnr lnr-arrow-right"></span>
-                            </button>
-                            <div style="position: absolute; left: -5000px;">
-                                <input type="text" name="b_1462626880ade1ac87bd9c93a_92a4423d01" tabindex="-1" value="">
-                            </div>
-                        </form>
-                    </div>
-                    <div class="info"></div>
-                </div>
-            </div>
-            <div class="col-lg-2 col-md-6 col-sm-6">
-                <div class="single-footer-widget">
-                    <h6>Instagram</h6>
-                    <div class="instagram-row">
-                        <a href="#"><img src="img/i1.jpg" alt=""></a>
-                        <a href="#"><img src="img/i2.jpg" alt=""></a>
-                        <a href="#"><img src="img/i3.jpg" alt=""></a>
-                        <a href="#"><img src="img/i4.jpg" alt=""></a>
-                        <a href="#"><img src="img/i5.jpg" alt=""></a>
-                        <a href="#"><img src="img/i6.jpg" alt=""></a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 col-sm-6">
-                <div class="single-footer-widget">
-                    <h6>Liên Hệ Với Chúng Tôi</h6>
-                    <p>ĐH Sài Gòn , <br>TP.HCM, VietNam</p>
-                    <p>
-                        <span class="lnr lnr-phone"></span> + 01 234 567 89<br>
-                        <span class="lnr lnr-envelope"></span> support@HKTC.com
-                    </p>
-                </div>
-            </div>
-        </div>
-        <div class="row footer-bottom d-flex justify-content-between align-items-center">
-            <p class="footer-text m-0 col-lg-6 col-md-6">
-                2024 © Mọi quyền được bảo lưu | Mẫu này được tạo với<i class="fa fa-heart" aria-hidden="true"></i> by
-                <a href="https://colorlib.com" target="_blank">HKTC</a>
-            </p>
-            <div class="col-lg-6 col-md-6 footer-social">
-                <a href="#"><i class="fa fa-facebook"></i></a>
-                <a href="#"><i class="fa fa-twitter"></i></a>
-                <a href="#"><i class="fa fa-dribbble"></i></a>
-                <a href="#"><i class="fa fa-behance"></i></a>
-            </div>
-        </div>
-    </div>
-</footer>
+<?php include 'footer.php'?>
 <!-- End footer Area -->
 </body>
 <!-- Modal Quick Product View -->
